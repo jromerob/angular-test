3894c6550d5e74836b66f9b611d66dfc49bcb3a5
Juan Carlos Romero <juancarlos.romero.blanco.contractor@bbva.com>
2022-10-12 11:01:13 +0200

    test a servicio con api mock

---
+++ src/app/services/book.service.spec.ts
@@ -0,0 +1,55 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
+import { Book } from '../models/book.model';
+import { BookService } from './book.service';
+
+const LIST_BOOKS: Book[] = [
+  { author: 'tres', isbn: '87583274', name: 'monbre3', price: 2, amount: 12 },
+  { author: 'dos', isbn: '87583274', name: 'monbre2', price: 25, amount: 1 },
+  { author: 'uno', isbn: '87583274', name: 'monbre1', price: 5, amount: 2 },
+];
+
+describe('Book Service', () => {
+  let service: BookService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BookService],
+    });
+  });
+
+  beforeEach(() => {
+    service = TestBed.inject(BookService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('Servicio creado', () => {
+    expect(service).toBeTruthy();
+  });
+
+  // public getBooks(): Observable<Book[]> {
+  //   const url: string = environment.API_REST_URL + `/book`;
+  //   return this._httpClient.get<Book[]>(url);
+  // }
+
+  it('devuelve lista de libros con un get', () => {
+    service.getBooks().subscribe((books: Book[]) => {
+      expect(books).toEqual(LIST_BOOKS);
+    });
+
+    const req = httpMock.expectOne(environment.API_REST_URL + '/book');
+    expect(req.request.method).toBe('GET');
+    // resuelve la request retornando un valor
+    req.flush(LIST_BOOKS);
+  });
+});
