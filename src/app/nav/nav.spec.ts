cebfc04da256ab77653b881f8ad0342128a40bec
Juan Carlos Romero <juancarlos.romero.blanco.contractor@bbva.com>
2022-10-16 20:59:16 +0200

    test router con routermock

--- src/app/nav/nav.spec.ts
+++ src/app/nav/nav.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
 import { NavComponent } from './nav.component';
 
 //clase para no tener que importar los componewntes a los que navegaar y toodas sus dependencias
-class ComponentTestRoute {}
+// class ComponentTestRoute {}
+const RouterMock = {
+  navigate() {},
+};
 
 describe('Nav Component', () => {
   let component: NavComponent;
@@ -13,19 +16,19 @@ describe('Nav Component', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule.withRoutes([
-          {
-            path: 'home',
-            component: ComponentTestRoute,
-          },
-          {
-            path: 'cart',
-            component: ComponentTestRoute,
-          },
-        ]),
+        // RouterTestingModule.withRoutes([
+        //   {
+        //     path: 'home',
+        //     component: ComponentTestRoute,
+        //   },
+        //   {
+        //     path: 'cart',
+        //     component: ComponentTestRoute,
+        //   },
+        // ]),
       ],
       declarations: [NavComponent],
-      providers: [],
+      providers: [{ provide: Router, useValue: RouterMock }],
     }).compileComponents();
   });
 
@@ -39,14 +42,11 @@ describe('Nav Component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should navigatge', () => {
+  it('should navigate', () => {
     const router = TestBed.inject(Router);
-    const spy1 = spyOn(router, 'navigate');
+    const spy = spyOn(router, 'navigate');
 
-    component.navTo('home');
-    expect(spy1).toHaveBeenCalledWith(['/home']);
-
-    component.navTo('cart');
-    expect(spy1).toHaveBeenCalledWith(['/cart']);
+    component.navTo('cualquiercosa');
+    expect(spy).toHaveBeenCalled();
   });
 });
